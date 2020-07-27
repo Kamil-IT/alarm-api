4a5e7c1622ddf329e544db67d498187fff6445a7
kkwol <kkwolny@vp.pl>
2020-07-27 15:22:19 +0200

    Clean up, delete not using class

--- src/main/java/com/clock/clockapi/AlarmFrequency.java
+++
@@ -1,9 +0,0 @@
-package com.clock.clockapi;
-
-
-import com.clock.clockapi.api.v1.model.alarm.frequency.AlarmFrequencyType;
-
-public interface AlarmFrequency {
-
-    AlarmFrequencyType getAlarmFrequencyType();
-}
