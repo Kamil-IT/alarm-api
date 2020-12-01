ac74724b8be61d9a267a98da3e5748ef6692400f
kamil-it <kkwolny@vp.pl>
2020-12-01 12:02:09 +0100

    Cleanup properties. Closed #13

--- src/main/java/com/clock/clockapi/security/config/SecurityConfigurerProd.java
+++ src/main/java/com/clock/clockapi/security/config/SecurityConfigurerProd.java
@@ -13,7 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Profile({"prod", "prod-missing-certificate"})
+@Profile({"prod", "prod-non-tls"})
 @EnableGlobalMethodSecurity(
         prePostEnabled = true,
         securedEnabled = true,
